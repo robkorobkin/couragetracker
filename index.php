c0ea95f19bcf3747fc09be0200432636b718f18d
Rob Korobkin <rob.korobkin@gmail.com>
2021-08-30 14:43:23 -0400

    FAVICON

--- index.php
+++ index.php
@@ -27,6 +27,21 @@
         <!-- LOAD APP STYLES -->
 		<link rel="stylesheet" href="client/theme/login.css?v=<?php echo time(); ?>">
 
+
+
+
+		<!-- FAVICON -->
+		<link rel="apple-touch-icon" sizes="180x180" href="client/theme/apple-touch-icon.png">
+		<link rel="icon" type="image/png" sizes="32x32" href="client/theme/favicon-32x32.png">
+		<link rel="icon" type="image/png" sizes="16x16" href="client/theme/favicon-16x16.png">
+		<link rel="manifest" href="client/theme/site.webmanifest">
+		<link rel="mask-icon" href="client/theme/safari-pinned-tab.svg" color="#5bbad5">
+		<meta name="msapplication-TileColor" content="#da532c">
+		<meta name="theme-color" content="#ffffff">
+
+
+
+
 		<script>
 			let mode = '<?php echo $mode; ?>'; 
 			let access_token = '<?php echo $access_token; ?>'; 
